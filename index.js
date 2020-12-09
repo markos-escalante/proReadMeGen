9335671d9da700b20b3a21562d62e0b2f324d69e
Markos Rafael <escalante.markos18@gmail.com>
2020-12-09 13:37:53 -0800

    create .gitignore file and include node_modules. Create basic coding outline for the prompt of questions that the application will ask.

---
+++ index.js
@@ -0,0 +1,18 @@
+const inquirer = require("inquirer")
+
+inquirer
+    .prompt([
+        
+    ])
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
