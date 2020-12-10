151d8b93fcd30d70e5522d9754f2c2f6ed46986b
Markos Rafael <escalante.markos18@gmail.com>
2020-12-09 23:03:58 -0800

    add the required questions to the command line application.

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const inquirer = require("inquirer")
-
+//enter a description, installation instructions, 
+//usage information, contribution guidelines, and test instructions
 inquirer
     .prompt([
         {
             type = "input",
-            name = "name",
-            message = "What is the title of your project?"
+            name = "input",
+            message = "What is the title of your project?",
         },
         {
-            type: "input",
-            name: "name",
-            message = ""
+            type = "input",
+            name = "description",
+            message = "Please enter a descripton of your project.",
+
+        },
+        {
+            type = "input",
+            name = "install",
+            message = "Please enter a brief description on how to install your application."
+        }, 
+        {
+            type = "input",
+            name = "usage information",
+            message = "Please enter a brief description on the usage information of your application."
+        },
+        {
+            type = "input",
+            name = "contribution guidelines",
+            message = "Please enter any contribution guidelines you may have for your application."
+        },
+        {
+            type = "input",
+            name  = "test instructions",
+            message = "Please enter a brief description detailing how to test your application",
+        },
+        {
+            type: "list",
+            message: "What licenses would you like to use?",
+            name: "licenses",
+            choices = ["mit", "apache 2.0", "wtfp1"],
+        },
+        {
+
         }
     ])
 
